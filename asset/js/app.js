a51f99c4a91772a5907a52a3e6b3d852dda830bb
IlyasaNandaRahmadianto <ilyasananda16@gmail.com>
2023-08-09 21:41:39 +0700

    update

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,11 +1,40 @@
 $(window).on("load", function () {
-  var elem = document.querySelector(".mansory");
-  var msnry = new Masonry(elem, {
+  const elem = document.querySelector(".mansory");
+  new Masonry(elem, {
     // options
     itemSelector: ".mansory-item",
     columnWidth: 200,
     gutter: 20,
     columnWidth: ".mansory-sizer",
     percentPosition: true,
+  });
+
+  $(".btn-to-top").click(function () {
+    $("html", "body").animate(
+      {
+        scrollTop: 0,
+      },
+      1500,
+      "easeInOutExpo"
+    );
+  });
+
+  AOS.init();
+
+  const portofolioIsotope = $(".portofolio-container").isotope({
+    itemSelector: ".portofolio-item",
+  });
+
+  $(".portofolio-filter li").click(function () {
+    $(".portofolio-filter li").removeClass("filter-active");
+    $(".portofolio-filter li").removeClass("text-white");
+    $(this).addClass("filter-active");
+    $(this).addClass("text-white");
+
+    portofolioIsotope.isotope({
+      filter: $(this).data("filter"),
+    });
+
+    AOS.init();
   });
 });
